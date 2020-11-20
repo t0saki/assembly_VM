422bf8755d77cda5b78516ac9fad39951c3acf8e
tosaki-up <wzx1459931670@outlook.com>
2020-11-20 16:24:55 +0800

    asm

---  demo.asm
+++  demo.asm
@@ -1,4 +1,51 @@
-INT A
+INT N
+IN N
+INT FL
+LD FL 2
+INT I
+INT J
+INT RNGI
+INT RNGJ
+MOV RNGI N
+INT TMP
+LD TMP 0
+ADD RNGI TMP
+LD TMP 1
+DIV RNGI TMP
+INT TMP1
+INT TMP2
+
+MV I RNGI
+istart:
+    LD J 1
+    jstart:
+        MOV TMP I
+        MUL TMP J
+        MOV TMP2 N
+        SUB TMP2 TMP
+        SUB TMP N
+        AND TMP TMP2
+        NOT TMP TMP
+        JC TMP ifend
+            LD FL 0
+            JMP endf
+        ifend:
+    forjend:
+    LD TMP 0
+    ADD J TMP
+    MOV TMP I
+    SUB TMP J
+    JC TMP jstart
+LD TMP 0
+SUB I TMP
+MOV TMP1 I
+SUB TMP1 TMP
+JC TMP1 istart
+endf:
+OUT I
+
+
+/**INT A
 IN A
 INT B
 IN B
@@ -28,14 +75,5 @@ JC B test
 ram:
 0 1
 1 2
+2 0
 
-INT N
-IN N
-INT FL
-INT I
-INT J
-INT RNGI
-INT RNGJ
-MOV RNGI N
-DIV RNGI 1
-ADD RNGI 0
